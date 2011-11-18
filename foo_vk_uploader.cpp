6366f4df185dd10410e459e1643adb7b6e1065c0
Duny <majorquake3@gmail.com>
2011-11-18 23:26:29 +0700

    Some minor changes

--- foo_vk_uploader.cpp
+++ foo_vk_uploader.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 
-DECLARE_COMPONENT_VERSION ("vk.com music uploader", "1.0.2",
+DECLARE_COMPONENT_VERSION ("vk.com Music Uploader", "1.0.2",
 "Allows uploading music to the vk.com social network from foobar2000.\n\n\
 (c) 2011 Efimenko Dmitriy (majorquake3@gmail.com)");
 
