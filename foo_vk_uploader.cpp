7aba2c22bd455a4d0bb60912c994bd1370a9f00c
Duny <majorquake3@gmail.com>
2011-11-20 13:54:48 +0700

    Fixed version number

--- foo_vk_uploader.cpp
+++ foo_vk_uploader.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 
-DECLARE_COMPONENT_VERSION ("vk.com Music Uploader", "1.0.2 beta",
+DECLARE_COMPONENT_VERSION ("vk.com Music Uploader", "1.0.3 beta",
 "Allows uploading music to the vk.com social network from foobar2000.\n\n\
 (c) 2011 Efimenko Dmitry (majorquake3@gmail.com)");
 
